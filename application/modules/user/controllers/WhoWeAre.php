641a3733286eabba3925cda3cdb67edf10adb945
kalyanyadav <dkalyanyadav@gmail.com>
2017-09-14 00:09:09 +0530

    who we are page

--- application/modules/user/controllers/WhoWeAre.php
+++ application/modules/user/controllers/WhoWeAre.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class WhoWeAre extends MX_Controller {
+class Whoweare extends MX_Controller {
 
 	/**
 	 * Index Page for this controller.
